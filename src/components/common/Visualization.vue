a25d359eb15a4a7ac24d113150bef69516e25868
Anser0111 <1052521358@qq.com>
2021-04-29 12:00:06 +0800

    1.0版本

--- src/components/common/Visualization.vue
+++ src/components/common/Visualization.vue
@@ -1,7 +1,31 @@
 <template>
   <div id="home">
-    <div id="main" style="width: 600px; height: 600px"></div>
-    <button @click="animation">变化</button>
+    <h1>{{ title }}</h1>
+    <div id="main" style="width: 600px; height: 400px"></div>
+    <hr />
+    <div id="input">
+      <div id="input1">
+        <div>{{ div1 }}</div>
+        <input type="text" v-model="count" />
+      </div>
+      <div id="input2">
+        <div>{{ div2 }}</div>
+        <input type="text" v-model="second" />
+      </div>
+      <div id="input3">
+        <div>{{ div3 }}</div>
+        <div v-for="(item, index) in parseInt(count)" :key="index">
+          <span>{{ item + ". " }}</span>
+          <input type="text" v-model="nameArr[index]" />
+          <input type="text" v-model="initArr[index]" />
+          <input type="text" v-model="endArr[index]" />
+        </div>
+      </div>
+    </div>
+    <div id="start">
+      <button @click="animation">开始</button>
+      <button @click="random">随机数据测试</button>
+    </div>
   </div>
 </template>
 
@@ -10,18 +34,24 @@ export default {
   name: "App",
   data() {
     return {
+      title: "数据可视化生成器",
+      div1: "请输入将要输入数据的组数：",
+      count: 4,
+      div2: "请输入动画持续的时长（秒）：",
+      second: 5,
+      div3: "请输入每组数据的名称，初始值，结束值：",
+      nameArr: [],
+      initArr: [],
+      endArr: [],
       myChart: {},
       option: {},
-      data: [],//要放数据的位置
-      jsonData: [
-        [100, 200, 300, 400, 500],
-        [200, 500, 600, 400, 900],
-      ],// 要放的数据
-      j: 0,
+      data: [], //要放数据的位置
+      flag: false,
+      color: [],
     };
   },
   methods: {
-    drawChart() {
+    initChart() {
       // 基于准备好的dom，初始化echarts实例
       this.myChart = this.$echarts.init(document.getElementById("main"));
       // 指定图表的配置项和数据
@@ -31,7 +61,7 @@ export default {
         },
         yAxis: {
           type: "category",
-          data: ["A", "B", "C", "D", "E"],
+          data: this.nameArr,
           inverse: true,
           animationDuration: 300,
           animationDurationUpdate: 300,
@@ -51,47 +81,62 @@ export default {
           },
         ],
         legend: {
-          show: true,
+          show: false, // 是否显示图例
         },
         animationDuration: 0, // 初始动画的时长
         animationDurationUpdate: 3000, // 数据更新动画的时长
         animationEasing: "linear",
         animationEasingUpdate: "linear",
       };
-      // 使用刚指定的配置项和数据显示图表。
-      this.myChart.setOption(this.option);
     },
     run() {
+      // 如果已经建立了echarts实例，则销毁。
+      if (this.flag) {
+        this.myChart.dispose();
+      }
+      this.data = this.initArr.map((item) => +item);
+      this.initChart();
+      this.myChart.setOption(this.option);
       let data = this.option.series[0].data;
       for (let i = 0; i < data.length; ++i) {
-        data[i] += this.jsonData[this.j][i];
+        data[i] = parseInt(this.endArr[i]);
       }
+      this.option.yAxis.max = this.count - 1;
+      this.option.animationDurationUpdate = this.second * 1000;
       this.myChart.setOption(this.option);
+      this.flag = true;
     },
     animation() {
       setTimeout(() => {
         this.run();
-        this.j++;
       }, 0);
-      let timer = setInterval(() => {
-        //设置边界，当数据用完时，不会报错。
-        if (this.jsonData.length > this.j) {
-          this.run();
-          this.j++;
-        } else {
-          clearInterval(timer);
-        }
-      }, 3000);
+    },
+    random() {
+      for (let i = 0; i < this.count; i++) {
+        this.nameArr[i] = Math.round(Math.random() * 100);
+        this.initArr[i] = Math.round(Math.random() * 2000);
+        this.endArr[i] = Math.round(Math.random() * 5000);
+      }
+      this.run();
+      for (let i = 0; i < this.count; i++) {
+        this.nameArr[i] = null;
+        this.initArr[i] = null;
+        this.endArr[i] = null;
+      }
     },
   },
   mounted() {
-    for (let i = 0; i < 5; ++i) {
-      this.data.push(Math.round(Math.random() * 2000));
-    }
-    this.drawChart();
+    this.random();
   },
 };
 </script>
 
 <style>
+button {
+  margin: 5px;
+}
+input {
+  margin-right: 5px;
+  margin-bottom: 2px;
+}
 </style>
